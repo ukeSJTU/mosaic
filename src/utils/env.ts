91607d0db3c0b4ca8b782d4d3d4bfc5eb8b14a47
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-11-26 23:24:14 +0800

    feat(auth): setup oauth proxy plugin
    This commit adds oAuthProxy plugin from better-auth. The same GitHub
    OAuth app can be used for both local development and production by
    dynamically setting the redirect URI based on the environment.

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,13 +3,16 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
+    BASE_URL: z
+      .url("BASE_URL must be a valid URL")
+      .min(1, "BASE_URL cannot be empty"),
+    PROXY_URL: z
+      .url("PROXY_URL must be a valid URL")
+      .min(1, "PROXY_URL cannot be empty"),
     DATABASE_URL: z
       .url("DATABASE_URL must be a valid URL")
       .min(1, "DATABASE_URL cannot be empty"),
     BETTER_AUTH_SECRET: z.string().min(1, "BETTER_AUTH_SECRET cannot be empty"),
-    BETTER_AUTH_URL: z
-      .url("BETTER_AUTH_URL must be a valid URL")
-      .min(1, "BETTER_AUTH_URL cannot be empty"),
     GITHUB_CLIENT_ID: z.string().min(1, "GITHUB_CLIENT_ID cannot be empty"),
     GITHUB_CLIENT_SECRET: z
       .string()
@@ -19,7 +22,8 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    BASE_URL: process.env.BASE_URL,
+    PROXY_URL: process.env.PROXY_URL,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   },
